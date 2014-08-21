b2f8bebcc4c6a81247d5c1d84bb9f26730614956
stefano-m1 <stefano.meschiari@gmail.com>
2014-08-21 10:21:52 -0500

    Fixed long titles

--- js/prepare.js
+++ js/prepare.js
@@ -3,10 +3,10 @@ var Markov = require('./markov'),
     _ = require('underscore');
 var readline = require('readline');
 
-var abstracts = new Markov(fs.readFileSync('abstracts.txt', {encoding: 'utf8' }),
+var abstracts = new Markov(fs.readFileSync('../data/abstracts.txt', {encoding: 'utf8' }),
                          2);
-var titles = new Markov(fs.readFileSync('titles.txt', {encoding: 'utf8' }),
-                        2);
+var titles = new Markov(fs.readFileSync('../data/titles.txt', {encoding: 'utf8' }),
+                        1);
 
 fs.writeFileSync('abstracts.json', JSON.stringify(abstracts));
 fs.writeFileSync('titles.json', JSON.stringify(titles));
